e25e84315dadade3f7fafc34a687d60212e71875
thaisandre <thaisandre.sc@gmail.com>
2024-11-30 04:14:59 -0300

    fix scale config

--- infra/main.tf
+++ infra/main.tf
@@ -110,8 +110,8 @@ resource "aws_eks_node_group" "tastytap_node_group" {
   subnet_ids      = module.vpc.private_subnets
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
 
